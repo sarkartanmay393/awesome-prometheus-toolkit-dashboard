e92bfb357dc114536e974bd165c7915395d694c0
Tanmay Sarkar <tanmay.sarkar@nirmata.com>
2024-11-27 00:17:52 +0530

    Lot of data stiching and used localstorage to persist data for sometime.

--- hooks/use-search.ts
+++ hooks/use-search.ts
@@ -23,10 +23,17 @@ export default function useSearch() {
     const fetchData = async () => {
       setInitialLoading && setInitialLoading(true);
       try {
+        const lastObj = LocalStorage.get('finalDataObject');
+        if (lastObj && Math.abs(Math.floor(new Date().getTime() / 1000) - Number(lastObj.timestamp)) < 999) {
+          setFilteredData(lastObj.value);
+          return;
+        }
         const response = await fetch(CONSTANTS.raw.rules);
         const text = await response.text();
         const data = yaml.load(text) as { groups: Group[] };
-        setFilteredData(formatAllData(data.groups as any));
+        const finalDataObject = await formatAllData(data.groups);
+        LocalStorage.set('finalDataObject', { timestamp: Math.floor(new Date().getTime() / 1000), value: finalDataObject });
+        setFilteredData(finalDataObject);
       } catch (error) {
         console.error("Error fetching data:", error);
       } finally {
@@ -44,16 +51,76 @@ export default function useSearch() {
   };
 }
 
-const formatAllData = (groups: any) => {
-  return groups?.map((group: any) => {
-    return {
+const LocalStorage = {
+  set: (key: string, value: any) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+  get: (key: string) => {
+    const value = localStorage.getItem(key);
+    if (value) {
+      return JSON.parse(value);
+    }
+    return null;
+  },
+  clear: () => {
+    localStorage.clear();
+  },
+};
+
+const formatAllData = async (groups: any) => {
+  const formattedGroups = await Promise.all(groups?.map(async (group: any) => {
+    const formattedGroup = {
       ...group,
-      services: group?.services?.map((service: any) => {
+      services: await Promise.all(group?.services?.map(async (service: any) => {
+        const exporterJson = await fetchDistRules(service);
         return {
           ...service,
-          description: service?.exporters?.[0]?.rules?.reduce((acc: string, curr: any) => acc+' '+curr.description, ''), 
+          description: service?.exporters?.[0]?.rules?.reduce(
+            (acc: string, curr: any) => `${acc} ${curr.description}`,
+            ""
+          ),
+          exporters: service?.exporters?.map((exporter: any) => {
+            const exporterName = exporter?.slug?.replace(/-/g, '').toLowerCase();
+            const matchingJson = exporterJson.find((json: any) => json?.name?.toLowerCase() === exporterName);
+            return {
+              ...exporter,
+              rules: exporter?.rules?.map((rule: any) => {
+                const ruleName = rule?.name?.replace(/\s+/g, '').toLowerCase();
+                const matchingRule = matchingJson?.rules?.find((r: any) => r?.alert?.toLowerCase() === ruleName);
+                return {
+                  ...rule,
+                  ...matchingRule,
+                };
+              }),
+            };
+          }),
         };
-      }),
+      })),
     };
-  });
+    return formattedGroup;
+  }));
+  return formattedGroups;
 };
+
+const constructRulePathNames = (service: any) => {
+  const serviceSlug = service?.name?.replace(/\s+/g, '-').toLowerCase();
+  const exporterSlugs = service?.exporters?.map((exporter: any) => exporter?.slug || "");
+  const ruleDirectory = `https://raw.githubusercontent.com/samber/awesome-prometheus-alerts/master/dist/rules/${serviceSlug}/`;
+  return exporterSlugs?.map((slug: string) => `${ruleDirectory}${slug}.yml`);
+};
+
+async function fetchDistRules(service: any) {
+  const rawPathsToDownload = constructRulePathNames(service);
+  try {
+    const responses = await Promise.all(rawPathsToDownload.map((path: string) => fetch(path)));
+    const responseTexts = await Promise.all(responses.map((response) => response.text()));
+    const jsons = responseTexts
+      .map((yamlText) => yaml.load(yamlText))
+      .map((json: any) => json?.groups)
+      .reduce((acc, curr) => [...acc, curr?.[0]], []);
+    return jsons;
+  } catch (error) {
+    console.error("Error fetching dist rules:", error);
+    return [];
+  }
+}
